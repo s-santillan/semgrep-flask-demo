ca0a86d1fdfc31572c9e9fd78eacb14b25bcb9b2
Nathan Brahms <nathan@returntocorp.com>
2021-07-24 10:13:40 -0700

    Update to use Marshmallow

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask, request, jsonify
+from marshmallow import Schema, fields, ValidationError
 
 app = Flask(__name__)
 
 
+# --- ERROR HANDLERS ---
+
+@app.errorhandler(ValidationError)
+def handle_validation_error(err: ValidationError):
+    return jsonify({"errors": err.normalized_messages()}), 400
+
+
+# --- SCHEMATA ---
+
+class DataSchema(Schema):
+    data = fields.Str(required=True)
+
+
+data_schema = DataSchema()
+
+# --- ROUTES ---
+
+
 @app.route('/', methods=['POST'])
 def reflect():
-    data = request.get_json()["data"]
+    data = data_schema.load(data=request.get_json())
     return jsonify(data)
